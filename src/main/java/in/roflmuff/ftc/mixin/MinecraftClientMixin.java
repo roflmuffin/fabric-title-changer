be607e144b29334881050550e34b83aaf9177c95
Roflmuffin <shortguy014@gmail.com>
2021-12-01 19:52:45 +1000

    Upgrade to minecraft 1.18

--- src/main/java/in/roflmuff/ftc/mixin/MinecraftClientMixin.java
+++ src/main/java/in/roflmuff/ftc/mixin/MinecraftClientMixin.java
@@ -4,10 +4,12 @@ import in.roflmuff.ftc.config.FabricTitleChangerConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.SharedConstants;
+import net.minecraft.client.ClientBrandRetriever;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.util.ModStatus;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,15 +19,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 @Environment(EnvType.CLIENT)
-public class MinecraftClientMixin {
+public class MinecraftClientMixin
+{
 	@Inject(at = @At(value = "RETURN"), method = "getWindowTitle", cancellable = true)
-	private void getWindowTitle(final CallbackInfoReturnable<String> info) {
-		MinecraftClient client = (MinecraftClient)(Object)this;
+	private void getWindowTitle(final CallbackInfoReturnable<String> info)
+	{
+		MinecraftClient client = (MinecraftClient) (Object) this;
 
-		if (FabricTitleChangerConfig.overrideFullTitle != null) {
+		if (FabricTitleChangerConfig.overrideFullTitle != null)
+		{
 
 			String returnVal = FabricTitleChangerConfig.overrideFullTitle;
-			if (client.getSession() != null) {
+			if (client.getSession() != null)
+			{
 				returnVal = returnVal.replaceAll("%username%", client.getSession().getUsername());
 			}
 
@@ -34,32 +40,46 @@ public class MinecraftClientMixin {
 		}
 
 		StringBuilder stringBuilder = new StringBuilder();
-		if (FabricTitleChangerConfig.overrideMinecraftTitle != null) {
+		if (FabricTitleChangerConfig.overrideMinecraftTitle != null)
+		{
 			stringBuilder.append(FabricTitleChangerConfig.overrideMinecraftTitle);
-		} else {
+		}
+		else
+		{
 			stringBuilder.append("Minecraft");
 		}
 
-		if (client.isModded() && FabricTitleChangerConfig.includeAsterisk) {
+		if (ModStatus.check("vanilla", ClientBrandRetriever::getClientModName, "Client", MinecraftClient.class).isModded() && FabricTitleChangerConfig.includeAsterisk)
+		{
 			stringBuilder.append("*");
 		}
 
-		if (FabricTitleChangerConfig.includeVersionNumber) {
+		if (FabricTitleChangerConfig.includeVersionNumber)
+		{
 			stringBuilder.append(" ");
 			stringBuilder.append(SharedConstants.getGameVersion().getName());
 		}
 
-		if (FabricTitleChangerConfig.includeNetworkStatus) {
+		if (FabricTitleChangerConfig.includeNetworkStatus)
+		{
 			ClientPlayNetworkHandler clientPlayNetworkHandler = client.getNetworkHandler();
-			if (clientPlayNetworkHandler != null && clientPlayNetworkHandler.getConnection().isOpen()) {
+			if (clientPlayNetworkHandler != null && clientPlayNetworkHandler.getConnection().isOpen())
+			{
 				stringBuilder.append(" - ");
-				if (client.getServer() != null && !client.getServer().isRemote()) {
+				if (client.getServer() != null && !client.getServer().isRemote())
+				{
 					stringBuilder.append(I18n.translate("title.singleplayer", new Object[0]));
-				} else if (client.isConnectedToRealms()) {
+				}
+				else if (client.isConnectedToRealms())
+				{
 					stringBuilder.append(I18n.translate("title.multiplayer.realms", new Object[0]));
-				} else if (client.getServer() == null && (client.getCurrentServerEntry() == null || !client.getCurrentServerEntry().isLocal())) {
+				}
+				else if (client.getServer() == null && (client.getCurrentServerEntry() == null || !client.getCurrentServerEntry().isLocal()))
+				{
 					stringBuilder.append(I18n.translate("title.multiplayer.other", new Object[0]));
-				} else {
+				}
+				else
+				{
 					stringBuilder.append(I18n.translate("title.multiplayer.lan", new Object[0]));
 				}
 			}
